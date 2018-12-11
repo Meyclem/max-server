9ad3cb7a5af181fe63e4670f1b255f519530c8ba
Clement Meyer <meyclem@gmail.com>
2018-12-11 17:04:25 +0100

    remove dotenv

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 // DEPENDENCIES IMPORT
 const OSC = require('osc-js')
 const express = require('express')
-require('dotenv').config()
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 ioClient = require('socket.io-client')
@@ -14,9 +13,8 @@ if (process.env.NODE_ENV === 'development') {
 } else {
   // This url will be used with yarn serve
   socket = ioClient.connect("https://nuxt-websocket.herokuapp.com/");
+  // socket = ioClient.connect("http://localhost:3000/");
 }
-
-socket.on('time', (data) => { console.log(data) })
 
 const HTTP_SERVER_PORT = 4000
 const OSC_SERVER_PORT = 9000
@@ -26,31 +24,40 @@ const config = {
   }
 }
 
-const app = express()
-
-app.listen(HTTP_SERVER_PORT, () => { console.log('HTTP server ready') })
-
-const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
-
 function sendMessage(data) {
   const message = new OSC.Message('/param/test', data)
   osc.send(message)
 }
 
+const app = express()
+const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
+
+app.listen(HTTP_SERVER_PORT, () => { console.log('HTTP server ready') })
+
 osc.on('open', () => { console.log('OSC server ready') })
 
 osc.on('error', err => { console.log('An error occurred', err) })
 
-osc.on('/param/mymsg', message => {
+osc.on('mouse', message => {
   console.log(message)
-  const msg = new OSC.Message('/param/test', message.args[0])
+  const msg = new OSC.Message('/param/fromsite', message.args[0])
   osc.send(msg)
 })
 
-setInterval(() => {
-  socket.emit('time', 'yoooooo')
-}, 1000)
-
 osc.on('close', () => { console.log('OSC server closed') })
 
-osc.open()
+socket.on('mouse', (message) => {
+  console.log(message)
+  const msg = new OSC.Message('/param/fromsite', message)
+  osc.send(msg)
+})
+
+socket.on('action', (tool) => {
+  console.log(tool)
+  const string = '/webapp-' + tool.join('-')
+  const msg = new OSC.Message('/param/fromsite', string)
+  osc.send(msg)
+})
+
+osc.open({ port: 8080 })
+
